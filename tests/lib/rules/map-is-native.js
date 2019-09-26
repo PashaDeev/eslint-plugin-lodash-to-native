6a334a96ff6129fe08e9d360a8af76f16e4a44c5
Deev Pavel <deev@uchi.ru>
2019-09-26 10:54:42 +0300

    add linting

--- tests/lib/rules/map-is-native.js
+++ tests/lib/rules/map-is-native.js
@@ -1,49 +1,48 @@
-const rule = require("../../../lib/rules/map-is-native");
-const RuleTester = require("eslint").RuleTester;
+const { RuleTester } = require('eslint');
+const rule = require('../../../lib/rules/map-is-native');
 
 const ruleTester = new RuleTester({ parserOptions: { ecmaVersion: 2015 } });
 
-const invalidCode = "const testArray = new Array(3);" +
-  "const result = _.map(testArray, (props) => {return props});" +
-  "const lastArr = [];";
+const invalidCode =
+  'const testArray = new Array(3);const result = _.map(testArray, (props) => {return props});const lastArr = [];';
 
-const varObj = 'const testObj = {a: 1};' +
-  '_.map(testObj, (prop) => prop)';
+const varObj = 'const testObj = {a: 1};_.map(testObj, (prop) => prop)';
 const objectCheck = '_.map({a: 1}, (prop) => prop)';
 
-  //todo проверка на переопределение
-  // "const _ = {map: function(){}};" +
-  // " _.map()";
+// todo проверка на переопределение
+// "const _ = {map: function(){}};" +
+// " _.map()";
 
-ruleTester.run("map-is-native", rule, {
+ruleTester.run('map-is-native', rule, {
   valid: [
     {
-      code: 'const variable = collection.map(fn)'
+      code: 'const variable = collection.map(fn)',
     },
     {
-      code: objectCheck
+      code: objectCheck,
     },
     {
-      code: varObj
-    }
+      code: varObj,
+    },
   ],
   invalid: [
     {
       code: 'const variable = _.map(collection, fn)',
-      output: 'const variable = Array.isArray(collection) ? collection.map(fn) : _.map(collection, fn)',
+      output:
+        'const variable = Array.isArray(collection) ? collection.map(fn) : _.map(collection, fn)',
       errors: [
         {
-          messageId: "preferNativeMap"
-        }
-      ]
+          messageId: 'preferNativeMap',
+        },
+      ],
     },
     {
       code: invalidCode,
       errors: [
         {
-          messageId: "preferNativeMap"
-        }
-      ]
-    }
-  ]
+          messageId: 'preferNativeMap',
+        },
+      ],
+    },
+  ],
 });
